991b264ccf7a635e4a3c7cc9f14db26d988873a9
Shabirahmad1676 <sa3835746@gmail.com>
2024-12-19 20:30:12 +0500

    Complete Redux with Todo App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todo from './components/Todo'
 
 function App() {
  
   return (
     <>
-    <h1>Shabir Ahmad</h1>
+    <h1>Learn About RTK</h1>
+    <AddTodo/>
+    <Todo/>
     </>
   )
 }
