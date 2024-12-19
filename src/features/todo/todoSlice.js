5598e8164cd6a0441c76323400f27125e36a6231
Shabirahmad1676 <sa3835746@gmail.com>
2024-12-19 19:44:09 +0500

    All about RTK

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,39 @@
+import { createSlice,nanoid } from "@reduxjs/toolkit";
+
+
+
+//this is state which is Object
+const initialState = {
+ todos:[{ id:1, text: 'hello' }]  
+}
+
+
+//Slice is nothing just huge virson of reducers
+//An action is an object that describes what changes need to be made to the state of your application. It sends data from your application to the Redux store and serves as the only way to update the store.
+
+
+
+export const todoSlice = createSlice({
+    name : 'todo',
+    initialState,
+    reducers:{
+      addTodo: (state,action)=>{
+        const todo={
+          id:nanoid(),
+          text: action.payload
+       }
+       state.todos.push(todo)
+      },
+    
+      removeTodo: (action,state)=>{
+        state.todos = state.todos.filter((todo)=>
+          todo.id !== action.payload
+        )
+      }
+    }
+})
+
+
+export const {addTodo,removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer
